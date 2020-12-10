5833c90adf824a983ba35e4ed46963308a226b30
eroux <elie.roux@telecom-bretagne.eu>
2020-12-10 10:31:41 +0100

    reorganize a bit

--- python/runtests.py
+++ python/runtests.py
@@ -8,14 +8,14 @@ def testonetranslation(uni, expected):
         print("converting %s : got %s but expected %s" % (uni, res, expected))
 
 def testtranlation():
-    with open('tests/dcl-simple.csv',  newline='') as csvfile:
+    with open('../tests/dcl-simple.csv',  newline='') as csvfile:
         srcreader = csv.reader(csvfile, delimiter=',')
         for row in srcreader:
             testtranslation(row[0], row[1])
 
 
 def testsegmentation():
-    with open('tests/syllableseg.txt',  newline='') as fp:
+    with open('../tests/syllableseg.txt',  newline='') as fp:
         for line in fp:
             expected = line.strip(' \n')
             base = expected.replace('_', '')
