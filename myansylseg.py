b83f7be294ee7710c609812a5e218cf99012cbe5
eroux <elie.roux@telecom-bretagne.eu>
2020-12-10 10:28:03 +0100

    implement syllable segmentation

--- myansylseg.py
+++ myansylseg.py
@@ -1,22 +1,138 @@
-import re
+# Implementation of the algorithm described in
+# Zin Maung Maung and Yoshiki Makami,"A rule-based syllable segmentation of Myanmar Text", in Proceeding of the IJCNLP-08 workshop of NLP for Less Privileged Language, January, 2008, Hyderabad, India, pp. 51-58
 
-# Initial code from https://github.com/ye-kyaw-thu/sylbreak/blob/master/python/sylbreak.py
-# Apache 2 license
-# converted to Python3
+# A = 0 = Asat
+# C = 1
+# D = 2
+# E = 3
+# F = 4
+# G = 5
+# I = 6
+# M = 7
+# P = 8
+# S = 9 = Virama
+# V = 10
+# W = 11
 
+CLASSES = {
+    "က": 1, "ခ": 1, "ဂ": 1, "ဃ": 1, "င": 1, "စ": 1, "ဆ": 1, "ဇ": 1, "ဈ": 1, "ဉ": 1, "ည": 1,
+    "ဋ": 1, "ဌ": 1, "ဍ": 1, "ဎ": 1, "ဏ": 1, "တ": 1, "ထ": 1, "ဒ": 1, "ဓ": 1, "န": 1, "ပ": 1,
+    "ဖ": 1, "ဗ": 1, "ဘ": 1, "မ": 1, "ယ": 1, "ရ": 1, "လ": 1, "ဝ": 1, "သ": 1, "ဟ": 1, "ဠ": 1,
+    "အ": 1, "ဢ": 1, 
+    "ဣ": 3, 
+    "ဤ": 6,
+    "ဥ": 3, "ဦ": 3,
+    "ဧ": 6,
+    "ဨ": 3, "ဩ": 3,
+    "ဪ": 6,
+    "ါ": 10, "ာ": 10, "ိ": 10, "ီ": 10, "ု": 10, "ူ": 10, "ေ": 10, "ဲ": 10,
+    "ဳ": 10, # not indicated in the article
+    "ဴ": 10, # not indicated
+    "ဵ": 10, # not indicated
+    "ံ": 4, "့": 4, "း": 4, 
+    "္": 9,
+    "်": 0,
+    "ျ": 7, "ြ": 7, "ွ": 7, "ှ": 7,
+    "ဿ": 5,
+    "၀": 2, "၁": 2, "၂": 2, "၃": 2, "၄": 2, "၅": 2, "၆": 2, "၇": 2, "၈": 2, "၉": 2,
+    "၊": 8, "။": 8,
+    "၌": 6, "၍": 6,
+    "၎": 3,
+    "၏": 6,
+    "ၐ": 1, "ၑ": 1, "ၒ": 1, "ၓ": 1, "ၔ": 1, "ၕ": 1, "ၖ": 1, "ၗ": 1, "ၘ": 1, "ၙ": 1,
+}
 
-# TODO: get the tests from https://github.com/swanhtet1992/ReSegment/blob/master/resegment.py
+AUTO = [
+# A = 0
+[ -1, 12, 1, 1, 0, -1, 1, 0, 1, 0, 0, 1],
+# C = 1
+[ 0, 13, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+# D = 2
+[ -1, 1, 0, 1, -1, -1, 1, -1, 1, -1, -1, 1],
+# E = 3
+[ -1, 13, 1, 1, 2, 0, 1, -1, 1, -1, 0, 1],
+# F = 4
+[ -1, 12, 1, 1, 2, -1, 1, -1, 1, -1, -1, 1],
+# G = 5
+[ -1, 1, 1, 1, 0, -1, 1, -1, 1, -1, 0, 1],
+# I = 6
+[ -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1],
+# M = 7
+[ 2, 13, 1, 1, 0, 0, 1, 0, 1, -1, 0, 1],
+# P = 8
+[ -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1],
+# S = 9
+[ -1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+# V = 10
+[ 2, 14, 1, 1, 0, 0, 1, -1, 1, -1, 0, 1],
+# W = 11
+[ -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 0],
+# AC = FC = 12
+[ 3, 1, 1, 1, 1, 1, 1, 15, 1, 1, 1, 1],
+# CC = EC = MC = 13
+[ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+# VC = 14
+[ 0, 1, 1, 1, 1, 1, 1, 15, 1, 0, 1, 1],
+# ACM = FCM = VCM = 15
+[ 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
 
-myConsonant = r"က-အ"
-enChar = r"a-zA-Z0-9"
-otherChar = r"ဣဤဥဦဧဩဪဿ၌၍၏၀-၉၊။!-/:-@[-`{-~\s"
-ssSymbol = r'္'
-ngaThat = r'င်'
-aThat = r'်'
+def getTokenClass(c):
+    if c in CLASSES:
+        return CLASSES[c]
+    return 11
 
-#Regular expression pattern for Myanmar syllable breaking
-#*** a consonant not after a subscript symbol AND a consonant is not followed by a-That character or a subscript symbol
-BreakPattern = re.compile(r"((?<!" + ssSymbol + r")["+ myConsonant + r"](?![" + aThat + ssSymbol + r"])" + r"|[" + enChar + otherChar + r"])")
+# -1: illegal character sequence
+#  0: no break after first character
+#  1: break after first character
+#  2: break after second character
+#  3: break after 4th character
 
-def addcharatsyllbreaks(s, c):
-	return BreakPattern.sub(c + r"\1", s).strip(c)
+def getNextBreak(s, idx):
+    state = None
+    curidx = idx
+    while curidx < len(s):
+        cl = getTokenClass(s[curidx:curidx+1])
+        if state is None:
+            state = cl
+            curidx += 1
+            idx = curidx
+            continue
+        res = AUTO[state][cl]
+        #print("%d + %d = %d " % (state, cl, res))
+        if res > 5:
+            state = res
+            curidx += 1
+            continue
+        if res == -1:
+            print("warning: illegal sequence")
+            state = cl
+            curidx += 1
+            idx = curidx
+            continue
+        if res == 0:
+            state = cl
+            curidx += 1
+            idx = curidx
+            continue
+        #print("return with res=%d, idx=%d" % (res, idx+res-1))
+        return idx+res-1
+    # no further break, but we go out of intermediate states
+    # by simulating a blank space afterwards
+    if state > 5:
+        res = AUTO[state][11]
+        if res > 0:
+            return idx+res-1
+    return -1
+
+def getTokens(s):
+    res = []
+    idx = 0
+    while idx < len(s):
+        nextidx = getNextBreak(s, idx)
+        if nextidx == -1:
+            res.append(s[idx:])
+            break
+        res.append(s[idx:nextidx])
+        idx = nextidx
+    return res
--- runtests.py
+++ runtests.py
@@ -2,21 +2,27 @@ import TranslitMyanmarOBI
 import myansylseg
 import csv
 
-def testtranslation(uni, expected):
+def testonetranslation(uni, expected):
     res = TranslitMyanmarOBI.translit(uni)
     if res != expected:
         print("converting %s : got %s but expected %s" % (uni, res, expected))
 
-with open('tests/dcl-simple.csv',  newline='') as csvfile:
-    srcreader = csv.reader(csvfile, delimiter=',')
-    for row in srcreader:
-        testtranslation(row[0], row[1])
+def testtranlation():
+    with open('tests/dcl-simple.csv',  newline='') as csvfile:
+        srcreader = csv.reader(csvfile, delimiter=',')
+        for row in srcreader:
+            testtranslation(row[0], row[1])
 
 
-with open('tests/syllableseg.txt',  newline='') as fp:
-    for line in fp:
-        expected = line.strip(' \n')
-        base = expected.replace('_', '')
-        res = myansylseg.addcharatsyllbreaks(base, '_')
-        if res != expected:
-            print("segmenting %s : got %s but expected %s" % (base, res, expected))
+def testsegmentation():
+    with open('tests/syllableseg.txt',  newline='') as fp:
+        for line in fp:
+            expected = line.strip(' \n')
+            base = expected.replace('_', '')
+            tokens = myansylseg.getTokens(base)
+            res = '_'.join(tokens)
+            if res != expected:
+                print("segmenting %s : got %s but expected %s" % (base, res, expected))
+            #break
+
+testsegmentation()
