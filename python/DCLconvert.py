424aecabc1c8b649cd297942d87588be2c640f12
eroux <elie.roux@telecom-bretagne.eu>
2020-12-15 15:48:04 +0100

    most tests pass now

--- python/DCLconvert.py
+++ python/DCLconvert.py
@@ -117,13 +117,13 @@ CLASSES = {
 #  0: no break after first character
 #  1: break after first character
 #  2: break after second character
-#  3: break after 4th character
+#  4: break after 4th character
 
 AUTO = [
 # A = 0
 [ -1, 12, 1, 1, 0, -1, 1, 0, 1, 0, 0, 1],
 # C = 1
-[ 0, 13, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+[ 0, 13, 1, 1, -2, 0, 1, 0, 1, 0, 0, 1],
 # D = 2
 [ -1, 1, 0, 1, -1, -1, 1, -1, 1, -1, -1, 1],
 # E = 3
@@ -133,9 +133,9 @@ AUTO = [
 # G = 5
 [ -1, 1, 1, 1, 0, -1, 1, -1, 1, -1, 0, 1],
 # I = 6
-[ -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1],
+[ -1, 14, 1, 1, 0, -1, 1, -1, 1, -1, -1, 1],
 # M = 7
-[ 2, 17, 1, 1, 0, 0, 1, 0, 1, -1, 0, 1],
+[ 2, 17, 1, 1, -2, 0, 1, 0, 1, -1, 0, 1],
 # P = 8
 [ -1, 1, 1, 1, -1, -1, 1, -1, 1, -1, -1, 1],
 # S = 9
@@ -155,7 +155,7 @@ AUTO = [
 # CCF = 16
 [ -2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 # EC = MC = 17
-[ 0, 1, 1, 1, 18, 1, 1, 1, 1, -2, 1, 1],
+[ -2, 1, 1, 1, 18, 1, 1, 1, 1, -2, 1, 1],
 # ECF = MCF = 18
 [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
@@ -170,6 +170,22 @@ def getNextTokenInfo(s, idx, slen):
         # hack: āu = āe = o
         if nexttwo == "ော" or nexttwo == "ာေ":
             return [2, [10, "o"]]
+        # hack: Unicode specifies 102D (i) 102F (u) but translitteration says ui
+        if nexttwo == "ို":
+            return [2, [10, "ui"]]
+        # hack: tone markers
+        if nexttwo == "အ်":
+            return [2, [4, "°a·"]]
+        if nexttwo == "ဝ်":
+            return [2, [4, "v·"]]
+        if nexttwo == "ဟ်":
+            return [2, [4, "h·"]]
+        if nexttwo == "့်" or nexttwo == "့်":
+            return [2, [0, "·."]]
+    if slen >= idx+3:
+        nextthree = s[idx:idx+3]
+        if nextthree == "္အ်":
+            return [3, [4, "'·"]]
     nextone = s[idx:idx+1]
     if nextone in CLASSES:
         return [1, CLASSES[nextone]]
@@ -180,9 +196,11 @@ def getNextTransBreak(s, idx):
     curidx = idx
     ress = ""
     resrest = ""
+    # the last replacement that made it into ress (not resrest)
     lastrepl = ""
     slen = len(s)
     print("slen = %d" % slen)
+    seenVowel = False
     while curidx < slen:
         print("start loop with curidx = %d : %s" % (curidx, s[curidx:]))
         cinfo = getNextTokenInfo(s, curidx, slen)
@@ -193,6 +211,22 @@ def getNextTransBreak(s, idx):
         nbchars = cinfo[0]
         cl = cinfo[1][0]
         repl = cinfo[1][1]
+        print(cl)
+        if cl in [10, 3, 6]:
+            print("seenVowel = true")
+            seenVowel = True
+        # hack: အာ = °ā
+        print("ress=%s" %ress)
+        if lastrepl == "'":
+            print("hacking အ")
+            if repl == 'ā' or repl == 'ai':
+                ress = ress[:-1]+"°"
+                lastrepl = "°"
+            elif cl == 0:
+                ress = ress[:-1]+"°a"
+                lastrepl = "°a"
+                seenVowel = True
+        print("ress=%s" %ress)
         if state is None:
             state = cl
             curidx += nbchars
@@ -201,13 +235,12 @@ def getNextTransBreak(s, idx):
             lastrepl = repl
             continue
         res = AUTO[state][cl]
-        print("trans = %s, resrest = %s , %d + %d = %d " % (ress, resrest, state, cl, res))
+        print("trans = %s, cl=%s, repl=%s, lastrepl=%s, resrest = %s , %d + %d = %d " % (ress, cl, repl, lastrepl, resrest, state, cl, res))
         if res > 5:
             print("test %s" % res)
             state = res
             curidx += nbchars
             resrest += repl
-            lastrepl = repl
             continue
         if res == -1:
             print("warning: illegal sequence")
@@ -219,17 +252,21 @@ def getNextTransBreak(s, idx):
             lastrepl = repl
             continue
         if res == -2:
-            print("test -2")
+            print("test -2, lastrepl=%s" % lastrepl)
             state = cl
             curidx += nbchars
             idx = curidx
             # the ' / ° dance of အ
             if lastrepl == "'":
                 # replacing the last repl by °:
-                res = res[:-1]+"°"
-            ress += 'a'+resrest+repl
+                ress = ress[:-1]+"°"
+            if not seenVowel:
+                ress += 'a'+resrest+repl
+            else:
+                ress += resrest+repl
             resrest = ""
             lastrepl = repl
+            seenVowel = True
             continue
         if res == 0:
             print("test 0")
@@ -240,17 +277,18 @@ def getNextTransBreak(s, idx):
             resrest = ""
             lastrepl = repl
             continue
-        print("return with trans=%s res=%d, idx=%d" % (ress, res, idx+res-1))
+        print("return with trans=%s res=%d, idx=%d, state=%d" % (ress, res, idx+res-1, state))
         # 'a' after consonnant (cl=1, )
-        if state == 1:
+        if res == 1:
+            print("test1111")
             # the ' / ° dance of အ
             if lastrepl == "'":
                 # replacing the last repl by °:
-                res = res[:-1]+"°"
-            ress += 'a'+resrest
+                ress = ress[:-1]+"°"
+            ress += 'a'
+            seenVowel = True
         else:
-            ress += resrest
-        ress = re.sub(r'\.·$', '·.', ress)
+            ress += resrest+repl
         print("return2 with trans=%s idx=%d" % (ress, idx+res-1))
         return [idx+res-1, ress]
     # no further break, but we go out of intermediate states
@@ -258,29 +296,32 @@ def getNextTransBreak(s, idx):
     print("end: trans=%s, resrest=%s, state=%d" % (ress, resrest, state))
     if state > 5:
         res = AUTO[state][11]
+        print("res = %d" %res)
         if res == -1:
             ress = ress+resrest
             return [-1, ress]
         if res == -2:
-            ress = ress+'a'+resrest
+            ress = ress+'a'#+resrest
+            seenVowel = True
             # the ' / ° dance of အ
             if lastrepl == "'":
                 # replacing the last repl by °:
-                res = res[:-1]+"°"
-            ress = re.sub(r'\.·$', '·.', ress)
+                ress = ress[:-1]+"°"
             return [idx+res-1, ress]
-        ress = ress+resrest
-        ress = re.sub(r'\.·$', '·.', ress)
+        ress = ress#+resrest
+        if not seenVowel:
+            ress += 'a'
         return [idx+res-1, ress]
     if state == 1:
-        ress += 'a'+resrest
         # the ' / ° dance of အ
         if lastrepl == "'":
             # replacing the last repl by °:
-            res = res[:-1]+"°"
+            ress = ress[:-1]+"°"
+        ress += 'a'+resrest
     else:
         ress += resrest
-    ress = re.sub(r'\.·$', '·.', ress)
+        if not seenVowel:
+            ress += 'a'
     return [-1, ress]
 
 def getTransTokens(s):
